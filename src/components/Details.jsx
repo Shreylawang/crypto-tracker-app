c09bd38e34980c5271e293489856905c4a3ffdff
Shrey Lawang <slawang108@gmail.com>
2022-03-17 17:02:42 +0530

    Atempt 02- failed

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,36 +1,40 @@
 import React from 'react'
 import { useParams } from 'react-router-dom';
 import { useState, useEffect } from 'react';
-export default function Details() {
 
+export default function Details()  {
+   // url param
+   const { id } = useParams();
   // Crypto data variable
   const [cryptos, setCryptos] = useState([]);
-
+  let User = '';
   //Getting Crypto data from api
   const getUsers = async () => {         
     const response = await fetch('https://api.coincap.io/v2/assets');
     const jsonResponse = await response.json();      
     setCryptos(jsonResponse.data); 
   };  
+  
   useEffect(() => {        
     getUsers();
   }, []);
-
-  console.log(cryptos)  
-  // url param
-  const { id } = useParams();
-  // console.log(id)
-
- 
-  function getCrypto(CryptoId) {
+  
+function getCrypto(CryptoId) {
     console.log(CryptoId)    
-    return cryptos.find(({id}) => CryptoId === id); 
+    return cryptos.find((CryptoId) => CryptoId.id === id); 
   }
-  const cryptoData = getCrypto(id);
-//  console.log(cryptoData)
+const matchUser = () => {
+    const temp = getCrypto(id);
+  const cryptoData = {temp}
+  console.log(cryptoData.temp); 
+  User = cryptoData.temp;  
+}
+
+matchUser()
+  console.log(User)
   return (
     <div>
-        <h1>{cryptoData}</h1>
+        <h1>{JSON.stringify(User)}</h1>
     </div>
   )
 }
